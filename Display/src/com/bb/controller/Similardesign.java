376516caf4106d37c2900100f7b2933437af30d6
shilpa <shilpay455@gmail.com>
2026-05-19 11:30:59 +0000

    new file added

---
+++ Display/src/com/bb/controller/Similardesign.java
@@ -0,0 +1,9 @@
+package com.bb.controller;
+
+public class Similardesign {
+	void  paintingnumber(String name) {
+		return;
+	}
+void displaypainting(String name) {
+	System.out.println("Displaying painting: " + name);
+}}
