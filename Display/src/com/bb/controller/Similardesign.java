209e41424f12d5b7d9a1f74c8ab2e73f840f278a
shilpa <shilpay455@gmail.com>
2026-05-19 11:54:21 +0000

    changes made

---
+++ Display/src/com/bb/controller/Similardesign.java
@@ -0,0 +1,12 @@
+package com.bb.controller;
+
+public class Similardesign {
+	String paintingnumber(String name) {
+		return "Painting number for " + name + " is 12345";
+	}
+	void displaypainting(String name) {
+		System.out.println("Displaying painting: " + name);
+	}
+
+	
+}
